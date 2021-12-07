8f35ee89caad7e8d9f47a9a44abc19a3635108db
Shay Kelley <smkelley91@gmail.com>
2021-12-07 13:21:20 -0600

    added some state

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [random, setRandom] = useState('')
   return (
     <div className="App">
       <header className="App-header">
